2a352de8ba3c897601955c1d7b0b8f7d1be01239
srino <seenivasan9559@gmail.com>
2021-03-29 12:33:22 +0530

    sa

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="blog">
-    <div class="py-8 md:py-16 text-center">
+    <div style="color:#fff;background-color:#c9302c;" class="py-8 md:py-16 text-center" >
       <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
         Slow-carb messenger bag mlkshk fingerstache four dollar toast.
