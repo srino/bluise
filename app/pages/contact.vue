e4869719dc01b4ae37f28cd3d595f9436d7be45a
srino <seenivasan9559@gmail.com>
2021-03-31 10:17:41 +0530

    saa

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -59,30 +59,16 @@
 import emailjs from 'emailjs-com';
 export default {
   name: 'ContactUs',
-  data() {
-    return {
-      name: '',
-      email: '',
-      message: ''
-    }
-  },
-  methods: {
-    sendEmail(e) {
-      try {
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target,'YOUR_USER_ID', {
-          name: this.name,
-          email: this.email,
-          message: this.message
-        })
 
-      } catch(error) {
-          console.log({error})
-      }
-      // Reset form field
-      this.name = ''
-      this.email = ''
-      this.message = ''
-    },
+  methods: {
+    sendEmail: (e) => {
+      emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        .then((result) => {
+            console.log('SUCCESS!', result.status, result.text);
+        }, (error) => {
+            console.log('FAILED...', error);
+        });
+    }
   }
 }
 </script>
